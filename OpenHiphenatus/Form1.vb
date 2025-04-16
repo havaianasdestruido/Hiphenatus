7e907ccdfbfb8e1e31b061088f282e7e2727d621
UltimateQuack <patoflamejantetv@gmail.com>
2025-04-16 12:42:01 -0300

    gkjhgkjhg

--- OpenHiphenatus/Form1.vb
+++ OpenHiphenatus/Form1.vb
@@ -6,19 +6,24 @@
     End Sub
 
     Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Hiphenatus.Form1.Shader1()
+        Hiphenatus.Form1.
+            Shader1()
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Hiphenatus.Form1.Shader2()
+        Hiphenatus.Form1.
+            Shader2()
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Hiphenatus.Form1.Shader3()
+        Hiphenatus.Form1.
+            Shader3()
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Hiphenatus.Form1.Shader4()
+        Hiphenatus.Form1.
+            Shader4()
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        Hiphenatus.Form1.Shader5()
+        Hiphenatus.Form1.
+            Shader5()
     End Sub
 End Class
